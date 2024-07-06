d731de6fe2923394a007c631f6bba50c6b591572
FreshCiabatta <131021786+FreshCiabatta@users.noreply.github.com>
2024-07-06 14:50:29 -0600

    Update connect.php
    Added code to connect to an unnamed database, will need to insert the correct info whenever we get a functioning database up. Also added needed code to  send the info that was piped from the web page written to the database

--- assets/php/connect.php
+++ assets/php/connect.php
@@ -4,5 +4,34 @@
   $email = $_POST['email'];
   $password = $_POST['password'];
 
-  //database connection
+  //database connection, uncomment after you read this. These are variables
+  //you will need to replace the strings with the appropriate information in order to connect with the database
+//$servername = "PLACE THE SERVER NAME HERE THIS";
+//$username = "DATABASE NAME HERE";
+//$password_db = "DATABASE PASSWORD HERE";
+//$dbname = "DATABASE NAME HERE";
+
+// create connection
+$conn = new mysqli($servername, $username, $password_db, $dbname);
+
+// check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// prepare and bind
+$stmt = $conn->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $name, $email, $password);
+
+// execute the statement
+if ($stmt->execute()) {
+    echo "SUCCESS, Recorded";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+// close the statement and connection
+$stmt->close();
+$conn->close();
+?>
 ?>
